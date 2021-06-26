3ff2d312f5ca7b2f231ddb5353b10578f4e049d8
Landry Kapela <landrykapela@gmail.com>
2021-06-26 18:33:56 +0300

    Styled Dashboard

--- js/main.js
+++ js/main.js
@@ -1,19 +1,36 @@
 const storage = window.localStorage;
 var currentUser = (storage.getItem("currentUser")) ? JSON.parse(storage.getItem("currentUser")):null;
 
-// const signup_url = "http://localhost:5000/signup";
-
 const signupForm = document.querySelector("#signup_form");
 if(signupForm){
-    signupForm.cpassword.addEventListener('change',(e)=>{
+    const fieldError = document.getElementById("field-error");
+    signupForm.cpassword.addEventListener('input',(e)=>{
         var password = signupForm.password.value;
         if(e.target.value === password) {
             signupForm.cpassword.classList.remove("fail-text");
             signupForm.cpassword.classList.add("primary-dark-text");
+            fieldError.classList.add("hidden");
         }
         else {
             signupForm.cpassword.classList.remove("primary-dark-text");
             signupForm.cpassword.classList.add("fail-text");
+            fieldError.textContent = "Passwords do not match";
+            fieldError.classList.remove("hidden");
+        }
+    });
+
+    signupForm.password.addEventListener('input',(e)=>{
+       
+        if(e.target.value.length >=8) {
+            signupForm.password.classList.remove("fail-text");
+            signupForm.password.classList.add("primary-dark-text");
+            fieldError.classList.add("hidden");
+        }
+        else {
+            signupForm.password.classList.remove("primary-dark-text");
+            signupForm.password.classList.add("fail-text");
+            fieldError.textContent = "Password is too short";
+            fieldError.classList.remove("hidden");
         }
     });
     signupForm.addEventListener('submit',(e)=>{
@@ -28,12 +45,11 @@ if(signupForm){
         }})
         .then(res=>res.json())
         .then(result=>{
-        window.location.pathname="/dashboard.html"
-        console.log("error: ",err);
-        const toast = document.createElement("div");
-        toast.classList.add("toast");
-        toast.textContent = err;
-        document.body.appendChild(toast);
+            console.log("signup ", result);
+            delete result.password;
+            currentUser = result;
+            storage.setItem("currentUser",JSON.stringify(currentUser));
+            showDashboard();
         });
     });
    
@@ -47,27 +63,26 @@ if(loginForm){
         let email = loginForm.email.value;
         let password = loginForm.password.value;
         let user = {email:email,password:password};
-        console.log("data: ",user);
-        // const signin_url = "http://localhost:5000/signin";
         fetch(signin_url,{method:"POST",body:JSON.stringify(user),headers:{'Content-type':'application/json'}})
         .then(res=>res.json()).then(response=>{
             if(response.error){
-                showErrorLogin(response.error);
+                showErrorFeedback(response.error);
             }
             else{
                 delete response.password;
                 currentUser = response;
                 storage.setItem("currentUser",JSON.stringify(currentUser));
-                window.location.pathname = "/dashboard.html";
+                showDashboard();
             }
         })
         .catch(err=>{
-            showErrorLogin(err.error);
+            let error = (err.error) ? err.error : "Connection Problems. Please try again later";
+            showErrorFeedback(error);
         });
     });
 }
 //show error login
-const showErrorLogin =(msg)=>{
+const showErrorFeedback =(msg)=>{
     const feedback = document.querySelector("#feedback");
     feedback.textContent = msg;
     feedback.classList.remove("hidden");
@@ -108,29 +123,66 @@ if(signout){
     });
 }
 
+//show dashboard
+const showDashboard = ()=>{
+    window.location.pathname = "/dashboard/";
+}
 //cpature client details
-const showClientDetailForm = (user)=>{
-    
+const showClientDetailForm = ()=>{
+    window.location.pathname = "/profile/";
+}
+const viewClientProfile = (user)=>{
+    const placeholder = document.querySelector("#placeholder");
+    while(placeholder.hasChildNodes()) placeholder.removeChild(placeholder.childNodes[0]);
+    const name = document.createElement("p");
+    name.textContent ="Company Name: "+ user.detail.company_name;
+    const email = document.createElement("p");
+    email.textContent = "Company Email: "+user.detail.email;
+    const address = document.createElement("p");
+    address.textContent ="Company Address: "+ user.detail.address;
+    const phone = document.createElement("p");
+    phone.textContent = "Company phone: "+user.detail.phone;
+    const contact_name = document.createElement("p");
+    contact_name.textContent ="Contact Person: "+ user.detail.contact_person;
+    const contact_email = document.createElement("p");
+    contact_email.textContent = "Contact Email: "+user.detail.contact_email;
+
+    placeholder.appendChild(name);
+    placeholder.appendChild(email);
+    placeholder.appendChild(address);
+    placeholder.appendChild(phone);
+    placeholder.appendChild(contact_name);
+    placeholder.appendChild(contact_email);
 }
 //activate user
 const activateAccount = (user)=>{
     var headers = {'Content-type':'application/json','Authorization':'Bearer '+user.accessToken};
     var body = JSON.stringify({email:user.email});
     fetch(initialize_url,{method:"POST",body:body,headers:headers})
-    .then(res=>json())
+    .then(res=>{
+        console.log(res);
+        if(res.status === 403){
+            alert("Your session has expired. Please sign in again");
+            signoutUser();
+        }
+        return res.json()})
     .then(result=>{
-        console.log("result: ",result);
+        showClientDetailForm();
     })
     .catch(er=>{
-        console.log("errr: ",err);
+        console.log("errr: ",er);
+        showErrorFeedback(err.msg);
     })
 }
 
 //check if current page is dashboard
-if(window.location.pathname == "/dashboard.html"){
+if(window.location.pathname == "/dashboard/"){
+    if(currentUser == null) window.location.pathname = "/signin.html";
+    else{
+         document.querySelector("#greetings").textContent = "Hello, "+currentUser.email;
     const activateButton = document.querySelector("#activate");
     const profileButton = document.querySelector("#profile");
-    if(currentUser.db == ""){
+    if(currentUser.db ==null){
         //handle activate button
         if(activateButton){
             activateButton.addEventListener('click',(e)=>{
@@ -145,8 +197,53 @@ if(window.location.pathname == "/dashboard.html"){
         if(profileButton){
             profileButton.addEventListener('click',(e)=>{
                 e.preventDefault();
-                showClientDetailForm(currentUser);
+                showClientDetailForm();
             })
         }
+    }
+    }
+   
+}
+
+//client profile
+if(window.location.pathname == "/profile/"){
+    const detailForm = document.querySelector("#client_profile_form");
+    detailForm.email.value = currentUser.email;
+    if(detailForm){
+        detailForm.addEventListener('submit',(e)=>{
+            e.preventDefault();
+
+            let name   = detailForm.company_name.value;
+            let email  = detailForm.email.value;
+            let phone  = detailForm.phone.value;
+            let person = detailForm.contact_person.value;
+            let cemail = detailForm.contact_email.value;
+            let address= detailForm.address.value;
+
+            let data = JSON.stringify({
+                company_name:name,
+                email:email,
+                phone:phone,
+                contact_person:person,
+                contact_email:cemail,
+                address:address
+            });
+
+            const headers = {
+                'Content-type':'application/json',
+                'Authorization':'Bearer '+currentUser.accessToken
+            }
+            const options = {
+                method:"POST",body:data,headers:headers
+            }
+            fetch(create_client_url,options)
+            .then(res=>res.json()).then(result=>{
+                showDashboard();
+            })
+            .catch(err=>{
+                console.log("err: ",err);
+            })
+
+        });
     }
 }
