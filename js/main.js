7b62b23cc0e63dbed60f9f8a48f8c35e9db649da
Landry Kapela <landrykapela@gmail.com>
2021-06-29 17:35:59 +0300

    Complete add client

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const storage = window.localStorage;
 var currentUser = (storage.getItem("currentUser")) ? JSON.parse(storage.getItem("currentUser")):null;
+var data = (storage.getItem("data")) ? JSON.parse(storage.getItem("data")):[];
 
+//arrow drop
+
+let arrowDropCount = document.getElementsByClassName("arrow").length;
+console.log('count: ',arrowDropCount);
 const signupForm = document.querySelector("#signup_form");
 if(signupForm){
     const fieldError = document.getElementById("field-error");
@@ -69,10 +74,10 @@ if(loginForm){
                 showErrorFeedback(response.error);
             }
             else{
-                delete response.password;
                 currentUser = response;
                 storage.setItem("currentUser",JSON.stringify(currentUser));
-                showDashboard();
+                if(currentUser.id == 0) showAdmin();
+                else showDashboard();
             }
         })
         .catch(err=>{
@@ -123,13 +128,21 @@ if(signout){
     });
 }
 
+//show admin dashboard
+const showAdmin = ()=>{
+    window.location.pathname = "/admin/";
+    // window.location.hash = "#"+target;
+}
 //show dashboard
 const showDashboard = ()=>{
     window.location.pathname = "/dashboard/";
 }
 //cpature client details
 const showClientDetailForm = ()=>{
-    window.location.pathname = "/profile/";
+    const clientList = document.querySelector("#clients_content");
+    const clientForm = document.querySelector("#add_client_content");
+    clientList.classList.add("hidden");
+    clientForm.classList.remove("hidden");
 }
 const viewClientProfile = (user)=>{
     const placeholder = document.querySelector("#placeholder");
@@ -175,6 +188,15 @@ const activateAccount = (user)=>{
     })
 }
 
+//check if admin is loggedin
+if(window.location.pathname ==="/admin/"){
+    if(currentUser == null || currentUser.id !== ADMIN){
+        window.location.pathname = "/signin.html";
+    }
+    else{
+        document.querySelector("#greetings").textContent = "Hello, Admin";
+    }
+}
 //check if current page is dashboard
 if(window.location.pathname == "/dashboard/"){
     if(currentUser == null) window.location.pathname = "/signin.html";
@@ -192,7 +214,10 @@ if(window.location.pathname == "/dashboard/"){
         }
     }
     else{
-        activateButton.classList.add("hidden");
+        if(activateButton){
+            activateButton.classList.add("hidden");
+        }
+        
         profileButton.classList.remove("hidden");
         if(profileButton){
             profileButton.addEventListener('click',(e)=>{
@@ -247,6 +272,61 @@ if(window.location.pathname == "/profile/"){
         });
     }
 }
+//client profile
+if(window.location.pathname == "/admin/"){
+    const detailForm = document.querySelector("#client_profile_form");
+    // detailForm.email.value = currentUser.email;
+    if(detailForm){
+        detailForm.addEventListener('submit',(e)=>{
+            e.preventDefault();
+
+            let name   = detailForm.company_name.value;
+            let email  = detailForm.email.value;
+            let phone  = detailForm.phone.value;
+            let person = detailForm.contact_person.value;
+            let cemail = detailForm.contact_email.value;
+            let address= detailForm.address.value;
+            let file = detailForm.company_logo.files[0];
+            let logoFile = null;
+            let data = {
+                company_name:name,
+                email:email,
+                phone:phone,
+                contact_person:person,
+                contact_email:cemail,
+                address:address,
+                logo:logoFile,
+                user:currentUser.id
+            };
+
+            if(file){
+                var reader = new FileReader();
+                reader.addEventListener('load',()=>{
+                    data.logo = reader.result;
+                },false);
+
+                reader.readAsDataURL(file);
+            }
+
+            const headers = {
+                'Content-type':'application/json',
+                'Authorization':'Bearer '+currentUser.accessToken
+            }
+            const options = {
+                method:"POST",body:JSON.stringify(data),headers:headers
+            }
+            fetch(create_client_url,options)
+            .then(res=>res.json()).then(result=>{
+                closeClientForm();
+                updateClients();
+            })
+            .catch(err=>{
+                console.log("err: ",err);
+            })
+
+        });
+    }
+}
 
 
 //handle sidebar nav
@@ -256,22 +336,212 @@ if(sideBar){
     items.forEach(item=>{
         if(item){
             item.addEventListener('click',(e)=>{
+                let target = e.target.id;
+                console.log(target);
                 items.forEach(i=>{
-                    if(i.classList.contains("active")) i.classList.remove("active");
+                    if(i.classList.contains("active")){
+                        i.classList.remove("active");
+                        document.getElementById(i.id+"_content").classList.add("hidden");
+                       
+                    } 
                 });
                 item.classList.add("active");
+                if(item.id =="clients") getClients();
+                else{
+                    document.getElementById("add_client_content").classList.add("hidden");
+                }
+                document.getElementById(target+"_content").classList.remove("hidden");
             })
         }
     })
     
 }
 
-//handle arrow drop down
-const arrowDrop = document.querySelector("#arrow-drop");
-if(arrowDrop){
-    const dropDown = document.querySelector("#drop-down");
-    arrowDrop.addEventListener('click',(d)=>{
-        if(dropDown.classList.contains("hidden")) dropDown.classList.remove("hidden");
-        else dropDown.classList.add("hidden");
+//show spinner
+const showSpinner=()=>{
+    const spinner = document.querySelector("#spinner");
+    if(spinner) spinner.classList.remove("hidden");
+}
+
+
+//hide spinner
+const hideSpinner=()=>{
+    const spinner = document.querySelector("#spinner");
+    if(spinner) spinner.classList.add("hidden");
+}
+//create row
+const createClientRow = (row)=>{
+    const holder = document.querySelector("#clients_content");
+    const rowHolder = document.createElement("div");
+    rowHolder.classList.add("body-row");
+    if(row == null){
+        const data = document.createTextNode("No clients");
+        rowHolder.appendChild(data);
+    }
+    else{
+        const companyName = document.createElement("span");
+        const companyAddress = document.createElement("span");
+        const contactName = document.createElement("span");
+        const contactEmail = document.createElement("span");
+        const companyPhone = document.createElement("span");
+
+        companyName.textContent = row.name;
+        companyAddress.textContent = row.address;
+        companyPhone.textContent = row.phone;
+        contactName.textContent = row.contact_person;
+        contactEmail.textContent = row.contact_email;
+
+        rowHolder.appendChild(companyName);
+        rowHolder.appendChild(companyAddress);
+        rowHolder.appendChild(companyPhone);
+        rowHolder.appendChild(contactName);
+        rowHolder.appendChild(contactEmail);
+    }
+
+    holder.appendChild(rowHolder);
+}
+//showClients
+const showClients = (data)=>{
+    if(data.length == 0){
+        createClientRow(null);
+    }
+    else{
+        const holder = document.querySelector("#clients_content");
+        Array.from(holder.children).forEach(child=>{
+            if(child.classList.contains("body-row")) holder.removeChild(child);
+        })
+        data.forEach(row=>{
+            createClientRow(row);
+        });
+    }
+}
+//show clientForm
+const closeClientForm=()=>{
+    document.querySelector("#add_client_content").classList.add("hidden");
+    document.querySelector("#clients_content").classList.remove("hidden");
+}
+//fetch clients
+const getClients = ()=>{
+    showSpinner();
+    const headers = {'Content-type':'application/json','Authorization':'Bearer '+currentUser.accessToken};
+    fetch(clients_url,{method:"GET",headers:headers})
+    .then(response=>{
+        if(response.status == 403){
+            showErrorFeedback("Your session has expired. Please login again");
+            setTimeout(()=>{
+                window.location.pathname = "/signin.html";
+            },3000);
+        }
+        else{
+        return response.json();
+        }
+        }).then(result=>{
+        hideSpinner();
+        data.clients = result.data;
+        storage.setItem("data",JSON.stringify(data));
+        showClients(result.data);
+    })
+    .catch(e=>{
+        hideSpinner();
+        showErrorFeedback("Your session has expired. Please login again");
+            
     })
 }
+
+//update clients
+const updateClients = (clients)=>{
+    if(clients && clients.length > 0){
+        data.clients = clients;
+        storage.setItem("data",JSON.stringify(clients));
+        showClients();
+    }
+}
+//handle arrow drop down
+for(let i=0;i<arrowDropCount;i++){
+    let id = "arrow-drop"+i;
+    const arrowDrop = document.getElementById(id);
+    console.log("test: ",arrowDrop.textContent);
+    if(arrowDrop){
+        arrowDrop.addEventListener('click',(e)=>{
+            const dropDown = document.querySelector("#drop-down"+i);
+            showHideDropDown(dropDown,arrowDrop);
+        })
+    }
+    
+}
+
+const showHideDropDown = (dropDown,arrowDrop)=>{
+    if(dropDown.classList.contains("hidden")) {
+        dropDown.classList.remove("hidden");
+        arrowDrop.innerHTML = "arrow_drop_up";
+    }
+    else {
+        dropDown.classList.add("hidden");
+        arrowDrop.innerHTML = "arrow_drop_down";
+    }
+}
+const sortClients = (sortBy,source)=>{
+    sortOrder = 0;
+    if(source.innerHTML == "arrow_drop_down") {
+        source.innerHTML = "arrow_drop_up";
+        sortOrder = 0;
+    }
+    else if(source.innerHTML == "arrow_drop_up") {
+        source.innerHTML = "arrow_drop_down";
+        sortOrder = 1;
+    }
+    let clients = data.clients;
+    if(clients.length > 0){
+        clients = clients.sort((a,b)=>{
+            let result = 0;
+            switch(sortBy){
+                case "sort_company":
+                    if(sortOrder == 0){
+                        result = (a.name < b.name) ? -1 : 1;
+                    }
+                    else result = (a.name < b.name) ? 1:-1;
+                    break;
+                case "sort_contact_name":
+                    if(sortOrder == 0){
+                        result = (a.contact_person < b.contact_person) ? -1 : 1;
+                    }
+                    else result = (a.contact_person < b.contact_person) ? 1:-1;
+                    break;
+                case "sort_contact_email":
+                if(sortOrder == 0){
+                    result = (a.contact_email < b.contact_email) ? -1 : 1;
+                }
+                else result = (a.contact_email < b.contact_email) ? 1:-1;
+                break;
+            }
+            return result;
+        })
+    }
+    
+    showClients(clients);
+}
+//search client
+const searchClients = (keyword)=>{
+    keyword = keyword.toLowerCase();
+    let clients = data.clients;
+    if(clients.length > 0){
+        clients = clients.filter(client=>{
+            return client.name.toLowerCase().includes(keyword) || 
+            client.contact_person.toLowerCase().includes(keyword) || 
+            client.contact_email.toLowerCase().includes(keyword) ||
+            client.region.toLowerCase().includes(keyword) ||
+            client.address.toLowerCase().includes(keyword)
+        })
+    }
+    return clients;
+}
+//search
+const search = document.querySelector("#search_client");
+if(search){
+    search.addEventListener('input',(e)=>{
+        showClients(searchClients(e.target.value));
+    })
+}
+
+
+//show client 
